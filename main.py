6a58ed3b60e5d620a4449614cf875ac82e1f2010
Bertram Aakjær <89650492+BertramAakjaer@users.noreply.github.com>
2024-04-09 18:46:59 +0200

    Added comments to clean code

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ from sub_functions.dup_checker import compare_files
 #   Streamlit UI
 def main():
     st.title("Folder Comparison Tool")
-
+    
     st.header("Select Folders")
     check_folder = st.text_input("Folder with duplicated files:")
     dump_folder = st.text_input("Dumping folder for the duplicates:")
 
     start_button = st.button("Start duplicate check")
 
+    # When start button is pressed: check if folders are valid and the proceed to call "compare files" function with the folders as args
     if start_button:
         if (not os.path.exists(Path(check_folder)) or not os.path.exists(Path(dump_folder))) or (check_folder == "" or dump_folder == ""):
             st.error("Please enter valid folder paths!")
@@ -32,6 +33,7 @@ def main():
             curr_file_text = st.empty()
             curr_file_text.text("Waiting for file to delete...")
             
+            # Calls function to compare files in givin folders
             compare_files(check_folder, dump_folder, progress_bar, time_text, curr_file_text)
 
 if __name__ == "__main__":
--- sub_functions/dup_checker.py
+++ sub_functions/dup_checker.py
@@ -9,21 +9,23 @@ from sub_functions.relative_paths import list_files
 
 def compare_files(primary_folder, destination_folder, progress_bar, time_left, currently_deleted_file):
     
+    # Calls function to create list over files in folder and subfolders
     total_byte_size, file_list = list_files(primary_folder)
     
-    unique_MD5s = dict()
+    unique_MD5s = dict() # Prepares an empty dictionary for unique MD5 hashes
 
     processed_data = 0
-    
     deleted_files = 0
     
+    # Gets current timecode
     start_timecode = datetime.now().strftime('%H:%M:%S.%f')
 
-
+    # Runs for every file in the list
     for file in file_list:
         progress_data = processed_data/total_byte_size
         progress_bar.progress(progress_data)
         
+        # Skips the first file, but then shows a time estiamte to complete process
         if file != file_list[0]:
             time_estimate = datetime.now().strftime('%H:%M:%S.%f')
             
@@ -38,20 +40,23 @@ def compare_files(primary_folder, destination_folder, progress_bar, time_left, c
         full_path = os.path.join(primary_folder, file)
         processed_data += os.path.getsize(full_path)
 
+        # Runs if file excists
         file_pathlib = Path(full_path)
         if file_pathlib.is_file():
 
             fileHash = hashlib.md5(open(file_pathlib, 'rb').read()).hexdigest()
 
+            # Checks if the hash has already been computed
             if fileHash not in unique_MD5s:
+                # Adds hash to list of computed unique hashes
                 unique_MD5s[fileHash] = file_pathlib
 
-            else:
+            else: # Makes a copy of the file into the dump folder and deletes the original
                 new_path = os.path.join(destination_folder, file)
                 
+                # Generates folder structure if non excistnt
                 temp_path = os.path.split(new_path)[0]
                 try:
-                    # Create the directory structure recursively, handling potential permission errors
                     os.makedirs(temp_path, exist_ok=True)
                 except OSError as error:
                     print(f"Error creating folder structure: {error}")
@@ -62,7 +67,8 @@ def compare_files(primary_folder, destination_folder, progress_bar, time_left, c
                 deleted_files += 1
         else:
             print("Path not exits")
-            
+    
+    # Shows user that the task is done and how many files was deleted
     st.write(str(deleted_files) + " files deleted!")
 
 # Calculate a string in the format "%H:%M:%S.%f" to a float of seconds
